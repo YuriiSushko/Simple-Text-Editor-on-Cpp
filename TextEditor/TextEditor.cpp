8a4f39991dbb0b53a3eccc652200292aea905f09
yurii_sushko <144662854+YuriiSushko@users.noreply.github.com>
2024-06-11 14:44:57 +0300

    everything done

--- TextEditor/TextEditor.cpp
+++ TextEditor/TextEditor.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <limits>
 #include <tuple>
+#include <stack>
 using namespace std;
 
 struct CharPointer {
@@ -88,6 +89,8 @@ private:
     CharPointer* dynamicArray;
     size_t capacity;
     int length;
+    stack<CharPointer*> undoStack;
+    stack<CharPointer*> redoStack;
 
 public:
     MyTextEditor() : capacity(64), length(0) {
@@ -98,8 +101,36 @@ public:
         }
     }
 
+    void clear_stack(stack<CharPointer*>& stackToClear) {
+        while (!stackToClear.empty()) {
+            free(stackToClear.top());
+            stackToClear.pop();
+        }
+    }
+
     ~MyTextEditor() {
         free(dynamicArray);
+        
+        clear_stack(undoStack);
+        clear_stack(redoStack);
+    }
+
+    void save_comand(stack<CharPointer*>& stack_to_save) {
+        CharPointer* comand = (CharPointer*)calloc(capacity, sizeof(CharPointer));
+        if (!comand) {
+            perror("Failed to allocate");
+            exit(EXIT_FAILURE);
+        }
+        memcpy(comand, dynamicArray, capacity * sizeof(CharPointer));
+        stack_to_save.push(comand);
+    }
+
+    void restore_comand(stack<CharPointer*>& stack_to_restore) {
+        if (!stack_to_restore.empty()) {
+            free(dynamicArray);
+            dynamicArray = stack_to_restore.top();
+            stack_to_restore.pop();
+        }
     }
 
     void print(int cursor_position = -1) const {
@@ -115,18 +146,22 @@ public:
         cout << std::endl;
     }
 
-    void to_realloc() {
+    void to_realloc()
+    {
         capacity *= 2;
+        
         CharPointer* tempArray = (CharPointer*)realloc(dynamicArray, capacity * sizeof(CharPointer));
         if (!tempArray) {
             perror("Failed to reallocate");
             free(dynamicArray);
             exit(EXIT_FAILURE);
-        }
+        }  
         dynamicArray = tempArray;
     }
 
     void appendText(bool ifItNewLine, int start_position = -1) {
+        save_comand(undoStack);
+        save_comand(redoStack);
         int ch;
         static int index = 0;
 
@@ -195,12 +230,14 @@ public:
         }
     }
 
-    void importFromFile() {
-        char loadstring[300];
+    int importFromFile() {
+        char loadstring[100];
         char fileNameToImport[64];
+        int line = 0;
 
         cout << "Enter a filename you want to import data from: ";
         cin >> fileNameToImport;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         FILE* fileImport;
         fopen_s(&fileImport, fileNameToImport, "r");
@@ -209,7 +246,7 @@ public:
             perror("Error opening file");
         }
         else {
-            while (fgets(loadstring, 100, fileImport) != NULL) {
+            while (fgets(loadstring,100, fileImport) != NULL) {
                 int lineLength = static_cast<int>(strlen(loadstring));
 
                 for (int i = 0; i < lineLength; i++) {
@@ -227,17 +264,20 @@ public:
 
                     if (loadstring[i] == '\n') {
                         dynamicArray[length].p = 0;
+                        line += 1;
                     }
 
                     dynamicArray[length].ch = '\0';
                 }
-                cout << loadstring;
             }
             fclose(fileImport);
         }
+        return line;
     }
 
     void Insert_with_replacment(int position) {
+        save_comand(undoStack);
+        save_comand(redoStack);
         int string_length;
 
         cout << "Enter the length of string:\n";
@@ -250,32 +290,50 @@ public:
         appendText(false, position);
     }
 
-    void insertByLineAndIndex() {
+    void insertByLineAndIndex(int position, CharPointer* bufer = NULL) {
+        save_comand(undoStack);
+        save_comand(redoStack);
         int ch;
-        Cursor cursor(&length);
+        if (bufer == NULL) {
+            cout << "Enter the text to insert:\n";
 
-        int line_to_insert = 0;
-        int column_to_insert = 0;
-        int position;
+            while ((ch = getchar()) != '\n') {
+                if (capacity - length > 1) {
+                    for (int j = length; j >= position; j--) {
+                        dynamicArray[j + 1].ch = dynamicArray[j].ch;
+                        dynamicArray[j + 1].p = dynamicArray[j].p;
+                    }
 
-        cout << "Enter line and column to insert\n";
-        cin >> line_to_insert >> column_to_insert;
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    dynamicArray[position].ch = ch;
+                    dynamicArray[position].p = dynamicArray[position - 1].p + 1;
 
-        cursor.set_cursor_position(make_tuple(line_to_insert, column_to_insert), dynamicArray);
-        position = cursor.get_position();
+                }
+                else {
+                    to_realloc();
 
-        cout << "Enter the text to insert:\n";
+                    for (int j = length; j >= position; j--) {
+                        dynamicArray[j + 1].ch = dynamicArray[j].ch;
+                        dynamicArray[j + 1].p = dynamicArray[j].p;
+                    }
+                    dynamicArray[position].ch = ch;
+                    dynamicArray[position].p = dynamicArray[position - 1].p + 1;
 
-        while ((ch = getchar()) != '\n') {
+                }
+
+                position++;
+                length++;
+            }
+        }
+        else {
             if (capacity - length > 1) {
                 for (int j = length; j >= position; j--) {
                     dynamicArray[j + 1].ch = dynamicArray[j].ch;
                     dynamicArray[j + 1].p = dynamicArray[j].p;
                 }
 
-                dynamicArray[position].ch = ch;
-                dynamicArray[position].p = dynamicArray[position-1].p + 1;
+                dynamicArray[position].ch = bufer[position].ch;
+                dynamicArray[position].p = dynamicArray[position - 1].p + 1;
+
             }
             else {
                 to_realloc();
@@ -284,9 +342,9 @@ public:
                     dynamicArray[j + 1].ch = dynamicArray[j].ch;
                     dynamicArray[j + 1].p = dynamicArray[j].p;
                 }
+                dynamicArray[position].ch = bufer[position].ch;
+                dynamicArray[position].p = dynamicArray[position - 1].p + 1;
 
-                dynamicArray[position].ch = ch;
-                dynamicArray[position].p = dynamicArray[position - 1].p + 1;;
             }
 
             position++;
@@ -295,17 +353,28 @@ public:
 
         dynamicArray[length].ch = '\0';
         return;
-        
-
     }
 
-    void delete_chars(int start_position) {
-        int num_chars_to_delete = 0;
-        cout << "Enter number of chars you want to delate\n";
-        cin >> num_chars_to_delete;
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    void delete_chars(int start_position, int num_chars_to_delete = -1) {
+        save_comand(undoStack);
+        save_comand(redoStack);
+        int new_length = 0;
 
-        int new_length = length - num_chars_to_delete;
+        if (num_chars_to_delete == -1) {
+            cout << "Enter number of chars you want to delete\n";
+            cin >> num_chars_to_delete;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            if (length - start_position < num_chars_to_delete) {
+                new_length = length - (length - start_position);
+            }
+            else {
+                new_length = length - num_chars_to_delete;
+            }
+        }
+        else {
+            new_length = 1;
+        }
+
         if (new_length < start_position) {
             new_length = start_position;
         }
@@ -386,6 +455,54 @@ public:
         free(tempArray);
     }
 
+    void undo() {
+        if (!undoStack.empty()) {
+            save_comand(redoStack);
+            restore_comand(undoStack);
+            cout << "Undo successfully\n";
+        }
+        else {
+            cout << "There is nothing to undo\n";
+        }
+    }
+
+    void redo() {
+        if (!redoStack.empty()) {
+            save_comand(undoStack);
+            restore_comand(redoStack);
+            cout << "Redo successfully\n";
+        }
+        else {
+            cout << "There is nothing to redo.\n";
+        }
+    }
+
+    CharPointer* copy(int startpoint, int number_of_symbols) {
+        int bufer_index = 0;
+        CharPointer* bufer = (CharPointer*)calloc(capacity, sizeof(CharPointer));
+        for (int i = startpoint; i <= (startpoint + number_of_symbols); i++) {
+            bufer[bufer_index] = dynamicArray[i];
+            bufer_index++;
+        }
+
+        bufer[bufer_index].ch = '\0';
+        bufer[bufer_index].p = bufer_index;
+        bufer_index++;
+        return bufer;
+    }
+
+
+    CharPointer* cut(int startpoint, int number_of_symbols) {
+        CharPointer* bufer = (CharPointer*)calloc(capacity, sizeof(CharPointer));
+        bufer = copy(startpoint, number_of_symbols);
+        delete_chars(startpoint, number_of_symbols);
+        return bufer;
+    }
+
+    void paste(int startpoint, CharPointer* bufer) {
+        insertByLineAndIndex(startpoint, bufer);
+    }
+
     void showCommands() const {
         cout << "^1 - Append text symbols to the end\n"
             << "^2 - Start the new line\n"
@@ -399,9 +516,13 @@ public:
             << "^q - quit from program\n";
     }
 
-    void run() {
+    void run(bool if_undo = false) {
         int command;
+        int line_to_insert = 0;
+        int column_to_insert = 0;
         int line = 0;
+        int number_of_symbols = 0;
+        CharPointer* bufer = (CharPointer*)calloc(capacity, sizeof(CharPointer));
 
         cout << "Welcome to my simple text editor\nEnter 8 to print the command list\n";
         
@@ -419,15 +540,15 @@ public:
             } 
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-            if (command == 11) {
+            if (command == -1) {
                 break;
             }
 
             switch (command) {
-            case 13:
+            case 22:
                 cursor.move_right();
                 break;
-            case 14:
+            case 33:
                 cursor.move_left();
                 break;
             case 8:
@@ -459,7 +580,8 @@ public:
                 break;
             case 5:
                 cout << "\033[H\033[J";
-                importFromFile();
+                line += importFromFile();
+                cursor.set_cursor_position(make_tuple(line, dynamicArray[length].p), dynamicArray);
                 break;
             case 6:
                 cout << "\033[H\033[J";
@@ -471,10 +593,48 @@ public:
                 break;
             case 9:
                 cout << "\033[H\033[J";
-                insertByLineAndIndex();
+
+                cout << "Enter line and column to insert\n";
+                cin >> line_to_insert >> column_to_insert;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                cursor.set_cursor_position(make_tuple(line_to_insert, column_to_insert), dynamicArray);
+                insertByLineAndIndex(cursor.get_position());
             case 10:
                 cout << "\033[H\033[J";
                 delete_chars(cursor.get_position());
+                break;
+            case 11:
+                cout << "\033[H\033[J";
+                undo();
+                break;
+            case 12:
+                cout << "\033[H\033[J";
+                redo();
+                break;
+            case 13:
+                cout << "\033[H\033[J";
+                cout << "Please, enter the number of symbols to copy\n";
+                cin >> number_of_symbols;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                copy(cursor.get_position(), number_of_symbols);
+                cout << "Copied successfully!\n";
+                break;
+            case 14:
+                cout << "\033[H\033[J";
+                cout << "Please, enter the number of symbols to copy\n";
+                cin >> number_of_symbols;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                bufer = cut(cursor.get_position(), number_of_symbols);
+                cout << "Cutted successfully!\n";
+                break;
+            case 15:
+                cout << "\033[H\033[J";
+
+                paste(cursor.get_position(), bufer);
+                cout << "Pasted successfully!\n";
                 break;
             default:
                 cout << "Please enter the commands correctly and without ^ symbol\n";
